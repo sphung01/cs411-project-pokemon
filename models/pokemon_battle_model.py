44fce4eb67de17d9f66c53c87b854a5089b381a4
Steven Phung <sphung01@bu.edu>
2025-04-29 11:03:34 -0400

    Added necessary functions

--- models/pokemon_battle_model.py
+++ models/pokemon_battle_model.py
@@ -4,6 +4,8 @@ import os
 import time
 from typing import List
 
+from logger import configure_logger
+
 logger = logging.getLogger(__name__)
 configure_logger(logger)
 
@@ -11,3 +13,21 @@ class BattleModel:
     """
         A class that manages the battlefield where pokemons fight.
     """
+
+    def __init__(self):
+        pass
+
+    def battle(self) -> str:
+        pass
+
+    def clear_battlefield(self):
+        pass
+
+    def enter_battlefield(self):
+        pass
+
+    def get_pokemons(self) -> List[Pokemons]:
+        pass
+
+    def get_pokemon_skills(self, pokemon: Pokemons) -> float:
+        pass
