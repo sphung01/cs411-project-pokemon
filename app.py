a1c27cfd6344aa0cd7986223afd2c6e0c4b1ad84
Steven Phung <sphung01@bu.edu>
2025-04-29 22:17:54 -0400

    Added User Management

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from dotenv import load_dotenv
 from flask import Flask, jsonify, make_response, Response, request
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from flask_cors import CORS
+# from flask_cors import CORS
 
 from config import ProductionConfig
+
 from models.user_model import Users
-from models.team_model import TeamModel
-from models.battle_model import BattleModel
+from models.pokemon_model import Pokemons
+from models.pokemon_battle_model import BattleModel
 from db import db
-from utils.logger import configure_logger
+from models.logger import configure_logger
 
 load_dotenv()
 
@@ -36,16 +37,226 @@ def create_app(config_class=ProductionConfig):
             "message": "Authentication required"
         }), 401)
 
-    team_model = TeamModel()
-    battle_model = BattleModel()
+    pokemon_model = Pokemons()
+
+    ####################################################
+    #
+    # Healthchecks
+    #
+    ####################################################
 
     @app.route('/api/health', methods=['GET'])
     def healthcheck() -> Response:
+        """
+        Health check route to verify the service is running.
+
+        Returns:
+            JSON response indicating the health status of the service.
+
+        """
         app.logger.info("Health check endpoint hit")
         return make_response(jsonify({
             'status': 'success',
             'message': 'Service is running'
         }), 200)
+    
+    ##########################################################
+    #
+    # User Management
+    #
+    #########################################################
+
+    @app.route('/api/create-user', methods=['PUT'])
+    def create_user() -> Response:
+        """Register a new user account.
+
+        Expected JSON Input:
+            - username (str): The desired username.
+            - password (str): The desired password.
+
+        Returns:
+            JSON response indicating the success of the user creation.
+
+        Raises:
+            400 error if the username or password is missing.
+            500 error if there is an issue creating the user in the database.
+        """
+        try:
+            data = request.get_json()
+            username = data.get("username")
+            password = data.get("password")
+
+            if not username or not password:
+                return make_response(jsonify({
+                    "status": "error",
+                    "message": "Username and password are required"
+                }), 400)
+
+            Users.create_user(username, password)
+            return make_response(jsonify({
+                "status": "success",
+                "message": f"User '{username}' created successfully"
+            }), 201)
+
+        except ValueError as e:
+            return make_response(jsonify({
+                "status": "error",
+                "message": str(e)
+            }), 400)
+        except Exception as e:
+            app.logger.error(f"User creation failed: {e}")
+            return make_response(jsonify({
+                "status": "error",
+                "message": "An internal error occurred while creating user",
+                "details": str(e)
+            }), 500)
+        
+    @app.route('/api/login', methods=['POST'])
+    def login() -> Response:
+        """Authenticate a user and log them in.
+
+        Expected JSON Input:
+            - username (str): The username of the user.
+            - password (str): The password of the user.
+
+        Returns:
+            JSON response indicating the success of the login attempt.
+
+        Raises:
+            401 error if the username or password is incorrect.
+        """
+        try:
+            data = request.get_json()
+            username = data.get("username")
+            password = data.get("password")
+
+            if not username or not password:
+                return make_response(jsonify({
+                    "status": "error",
+                    "message": "Username and password are required"
+                }), 400)
+
+            if Users.check_password(username, password):
+                user = Users.query.filter_by(username=username).first()
+                login_user(user)
+                return make_response(jsonify({
+                    "status": "success",
+                    "message": f"User '{username}' logged in successfully"
+                }), 200)
+            else:
+                return make_response(jsonify({
+                    "status": "error",
+                    "message": "Invalid username or password"
+                }), 401)
+
+        except ValueError as e:
+            return make_response(jsonify({
+                "status": "error",
+                "message": str(e)
+            }), 401)
+        except Exception as e:
+            app.logger.error(f"Login failed: {e}")
+            return make_response(jsonify({
+                "status": "error",
+                "message": "An internal error occurred during login",
+                "details": str(e)
+            }), 500)
+        
+    @app.route('/api/logout', methods=['POST'])
+    @login_required
+    def logout() -> Response:
+        """Log out the current user.
+
+        Returns:
+            JSON response indicating the success of the logout operation.
+
+        """
+        logout_user()
+        return make_response(jsonify({
+            "status": "success",
+            "message": "User logged out successfully"
+        }), 200)
+    
+    @app.route('/api/change-password', methods=['POST'])
+    @login_required
+    def change_password() -> Response:
+        """Change the password for the current user.
+
+        Expected JSON Input:
+            - new_password (str): The new password to set.
+
+        Returns:
+            JSON response indicating the success of the password change.
+
+        Raises:
+            400 error if the new password is not provided.
+            500 error if there is an issue updating the password in the database.
+        """
+        try:
+            data = request.get_json()
+            new_password = data.get("new_password")
+
+            if not new_password:
+                return make_response(jsonify({
+                    "status": "error",
+                    "message": "New password is required"
+                }), 400)
+
+            username = current_user.username
+            Users.update_password(username, new_password)
+            return make_response(jsonify({
+                "status": "success",
+                "message": "Password changed successfully"
+            }), 200)
+
+        except ValueError as e:
+            return make_response(jsonify({
+                "status": "error",
+                "message": str(e)
+            }), 400)
+        except Exception as e:
+            app.logger.error(f"Password change failed: {e}")
+            return make_response(jsonify({
+                "status": "error",
+                "message": "An internal error occurred while changing password",
+                "details": str(e)
+            }), 500)
+        
+    @app.route('/api/reset-users', methods=['DELETE'])
+    def reset_users() -> Response:
+        """Recreate the users table to delete all users.
+
+        Returns:
+            JSON response indicating the success of recreating the Users table.
+
+        Raises:
+            500 error if there is an issue recreating the Users table.
+        """
+        try:
+            app.logger.info("Received request to recreate Users table")
+            with app.app_context():
+                Users.__table__.drop(db.engine)
+                Users.__table__.create(db.engine)
+            app.logger.info("Users table recreated successfully")
+            return make_response(jsonify({
+                "status": "success",
+                "message": f"Users table recreated successfully"
+            }), 200)
+
+        except Exception as e:
+            app.logger.error(f"Users table recreation failed: {e}")
+            return make_response(jsonify({
+                "status": "error",
+                "message": "An internal error occurred while deleting users",
+                "details": str(e)
+            }), 500)
+    
+    ##########################################################
+    #
+    # Pokemons
+    #
+    ##########################################################
+
 
     # More routes go here: /create-user, /login, /logout, /change-password
     # Then routes like /team/add, /team/list, /battle/start, etc.
