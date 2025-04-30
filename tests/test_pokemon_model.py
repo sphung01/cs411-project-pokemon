18a6135258ae5a16647a4f6661021f20dac1e94a
Steven Phung <sphung01@bu.edu>
2025-04-30 10:30:02 -0400

    Testing the pokemon model

--- tests/test_pokemon_model.py
+++ tests/test_pokemon_model.py
@@ -1,1 +1,10 @@
-# Test here
+import pytest
+
+from models.pokemon_model import Pokemons
+from models.pokemon_battle_model import BattleModel
+
+@pytest.fixture
+def pokemon_battle_model():
+    """Fixture to provide a new instance of RingModel for each test."""
+    return BattleModel()
+
