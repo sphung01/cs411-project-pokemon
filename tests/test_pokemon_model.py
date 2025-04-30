ea5b4f400a1a94b9afed9c08fa74f52276825599
Steven Phung <sphung01@bu.edu>
2025-04-30 10:32:10 -0400

    Added pytest fixtures

--- tests/test_pokemon_model.py
+++ tests/test_pokemon_model.py
@@ -8,3 +8,28 @@ def pokemon_battle_model():
     """Fixture to provide a new instance of RingModel for each test."""
     return BattleModel()
 
+@pytest.fixture
+def sample_pokemon1(session):
+    pokemon = Pokemons(
+        name="Pikachu",
+        attack=40.0,
+        defense=25.0
+    )
+    session.add(pokemon)
+    session.commit()
+    return pokemon
+
+@pytest.fixture
+def sample_pokemon2(session):
+    pokemon = Pokemons(
+        name="Staryu",
+        attack=45.0,
+        defense=30.0
+    )
+    session.add(pokemon)
+    session.commit()
+    return pokemon
+
+@pytest.fixture
+def sample_boxers(sample_pokemon1, sample_pokemon2):
+    return [sample_pokemon1, sample_pokemon2]
